ba66273555ca821ebfddcc3770cc237e87a7efcb
Brian Cook <brybcook@gmail.com>
2021-01-27 16:57:22 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,6 @@
-count=11
-while count >0 do 
-  count=count-1
-  puts "#{count}"
-  break if count==1 
-  puts "Happy New Year!"
+count=10
+while count >=1 do 
+  puts count
+  count-=1
 end
+puts "Happy New Year!"
